803adee4a0a739049bac0b0a8e6b280436c2fb0b
timComp3215 <33968140+timComp3215@users.noreply.github.com>
2018-06-17 12:41:38 +0100

    Initial MSP432 code with increased reverse bit addressing
    Will now test bit addressing with arrays larger than 256

---
+++ uart_FFT_csv/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../system_msp432p401r.c \
+../uart_FFT_csv.c 
+
+C_DEPS += \
+./system_msp432p401r.d \
+./uart_FFT_csv.d 
+
+OBJS += \
+./system_msp432p401r.obj \
+./uart_FFT_csv.obj 
+
+OBJS__QUOTED += \
+"system_msp432p401r.obj" \
+"uart_FFT_csv.obj" 
+
+C_DEPS__QUOTED += \
+"system_msp432p401r.d" \
+"uart_FFT_csv.d" 
+
+C_SRCS__QUOTED += \
+"../system_msp432p401r.c" \
+"../uart_FFT_csv.c" 
+
+
